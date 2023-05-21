cf7904eaf993eb236daa786f871b1cf3a6c0c408
srehrt <101404785+srehrt@users.noreply.github.com>
2023-05-21 15:27:08 +0800

    version-01

---
+++ src/components/custom/GithubSite.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="text-neutral-400">
+    <span>Star on</span>
+    <a href="https://www.baidu.com" target="_blank" class="text-blue-500">
+      baidu
+    </a>
+  </div>
+</template>
---
+++ src/store/modules/settings/helper.ts
@@ -0,0 +1,30 @@
+import { ss } from '@/utils/storage'
+
+const LOCAL_NAME = 'settingsStorage'
+
+export interface SettingsState {
+  systemMessage: string
+  temperature: number
+  top_p: number
+}
+
+export function defaultSetting(): SettingsState {
+  return {
+    systemMessage: 'None',
+    temperature: 0.7,
+    top_p: 1,
+  }
+}
+
+export function getLocalState(): SettingsState {
+  const localSetting: SettingsState | undefined = ss.get(LOCAL_NAME)
+  return { ...defaultSetting(), ...localSetting }
+}
+
+export function setLocalState(setting: SettingsState): void {
+  ss.set(LOCAL_NAME, setting)
+}
+
+export function removeLocalState() {
+  ss.remove(LOCAL_NAME)
+}
